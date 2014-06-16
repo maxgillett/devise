88967dcfb5a73ac41d4cbee27559582b6aa1423e
Max Gillett <max.gillett@gmail.com>
2014-06-16 01:48:03 -0400

    activatable fix

--- lib/devise/hooks/activatable.rb
+++ lib/devise/hooks/activatable.rb
@@ -5,6 +5,7 @@ Warden::Manager.after_set_user do |record, warden, options|
   if record && record.respond_to?(:active_for_authentication?) && !record.active_for_authentication?
     scope = options[:scope]
     warden.logout(scope)
-    throw :warden, scope: scope, message: record.inactive_message
+    options.merge!(:message => record.inactive_message)
+    throw :warden, options
   end
 end
